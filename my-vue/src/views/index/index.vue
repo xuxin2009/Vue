e49d67b431c27780ebc4536c3076f6afd502e506
xuxin2009 <724056979@qq.com>
2018-03-07 15:14:54 +0800

    第一次提交部分

---
+++ my-vue/src/views/index/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    欢迎来到人员管理系统
+    <footer-nav></footer-nav>
+  </div>
+
+</template>
+<script>
+  import FooterNav from '../../components/Footer'
+  export default {
+    components:{
+      FooterNav
+    }
+  }
+</script>
+
---
+++ my-vue/src/views/mamage/index.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="manage tc">
+    <button>新增</button>
+    <div class="input-area">
+      <input type="text" placeholder="请输入人员姓名">
+      <button>确定</button>
+    </div>
+      <table>
+        <tr>
+          <th>姓名</th>
+          <th>操作</th>
+        </tr>
+        <tr>
+          <td>张三</td>
+          <td><span>删除</span><span>编辑</span></td>
+        </tr>
+      </table>
+    <footer-nav></footer-nav>
+  </div>
+</template>
+<script >
+  import FooterNav from '../../components/Footer'
+  export default {
+    components:{
+      FooterNav
+    }
+  }
+</script>
+<style>
+
+
+</style>
